f4c69c33b6d133fee0eb1668381783c1782b5b92
alfeups <alfeups@hotmail.com>
2022-10-13 23:03:55 -0300

    triDu challenge

---
+++ README.md
@@ -0,0 +1,3 @@
+<h3>Repository to keep challenges using JS</h3>
+
+[tri-du](impulsojs\tri-du.js)
---
+++ impulsojs/tri-du.js
@@ -0,0 +1,14 @@
+var s = gets().split(" ");
+let A = parseInt(s[0]);
+let B = parseInt(s[1]);
+let C;
+if( A == B){
+  C = A;
+}
+else if( A > B){
+  C = A;
+}
+else {
+  C = B;
+}
+print(C);
