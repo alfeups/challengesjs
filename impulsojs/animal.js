b705ff8e5b8dec5ed68af1944a898e9abd9041cd
alfeups <alfeups@hotmail.com>
2022-10-18 22:28:27 -0300

    adding animal challenge

---
+++ impulsojs/animal.js
@@ -0,0 +1,36 @@
+let x = gets(); 
+let y = gets(); 
+let z = gets(); 
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "carnivoro")) {
+  print("aguia\n");
+}
+
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "onivoro")) {
+  print("pomba\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "onivoro")) {
+  print("homem\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "herbivoro")) {
+  print("vaca\n");
+}
+
+if ((x == "invertebrado") && (y == "inseto")  && (z == "hematofago")) {
+  print("pulga\n");
+}
+
+if ((x ==  "invertebrado") && (y == "inseto")  && (z == "herbivoro")) {
+  print("lagarta\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "hematofago")) {
+  print("sanguessuga\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "onivoro")) {
+  print("minhoca\n");
+}
