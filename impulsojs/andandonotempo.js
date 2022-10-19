99769698018f2a325a946f1eb9fb28f350479217
alfeups <alfeups@hotmail.com>
2022-10-18 22:34:55 -0300

    adding time travel challenge

---
+++ impulsojs/andandonotempo.js
@@ -0,0 +1,11 @@
+const line = gets().split("")
+const a = line[0];
+const b = line[1];
+const c = line[2];
+
+
+if (a + b == c || a + c == b || c + b == a  || a == b  || c == a ||  b == c) {
+    print('S');
+} else {
+    print('N');
+}
