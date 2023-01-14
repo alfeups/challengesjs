b4ec99d48a43bc83f6a2cf4ae6db735efc6bf08c
alfeups <alfeups@hotmail.com>
2023-01-14 00:43:24 -0300

    pink cerebro challenge

---
+++ jsdeveloper/PinkCerebro.js
@@ -0,0 +1,14 @@
+let N = parseInt(gets());
+let numeros =  gets().split(" ").map(Number);
+let multiplos = 0;
+
+
+for (let i = 2; i < 6; i++) {
+    numeros.forEach(numero => {
+        if(numero % i === 0 ) {
+            multiplos++;
+        }
+    })
+    print(`${multiplos} Multiplo(s) de ${i}`)
+    multiplos = 0;
+}
