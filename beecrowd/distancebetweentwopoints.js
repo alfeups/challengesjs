2ceeba32fc31a6f2236c355b911ca363e752623e
alfeups <alfeups@hotmail.com>
2023-01-19 21:44:46 -0300

    improve distance between two points challenge

--- beecrowd/distancebetweentwopoints.js
+++ beecrowd/distancebetweentwopoints.js
@@ -1,6 +1,9 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\r\n').join(' ').split(' ');
+var lines = input.split('\r\n');
 
-let distancia = Math.pow((lines[2] - lines[0]), 2) + Math.pow((lines[3] - lines[1]), 2);
+const [x1, y1] = lines[0].split(" ");
+const [x2, y2] = lines[1].split(" ");
 
-console.log(Math.sqrt(distancia).toFixed(4));
+const distancia = Math.sqrt((Math.pow(x2 -x1, 2) + Math.pow(y2 - y1, 2)));
+
+console.log(distancia.toFixed(4));
